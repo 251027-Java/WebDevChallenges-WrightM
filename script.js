219c982cb49c814d41373d471d98996783d73e9a
Marcus Wright <marcus03wright@icloud.com>
2025-11-19 14:08:02 -0600

    Completed task 9

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello from the external script!");
+function sayHello() {
+  alert("Hello from the external file!");
+}
+
+function sayGoodbye() {
+    alert("Goodbye from the external file!");
+}
+
+// Connect the function to the button
+let btn = document.getElementById("helloBtn");
+btn.addEventListener("click", sayHello);
+
+let goodbyeBtn = document.getElementById("goodbyeBtn");
+goodbyeBtn.addEventListener("click", sayGoodbye);
