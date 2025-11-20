1a64e2edb8de125c2e1d828112edeeb507741f8a
Marcus Wright <marcus03wright@icloud.com>
2025-11-20 11:11:17 -0600

    Complted task 22

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const url = "https://jsonplaceholder.typicode.com/posts/1";
 
-const makeObject = (title, author) => ({ title, author});
-
-console.log(makeObject("Book Title", "Author Name"));
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('output').innerHTML = `<h2>${data.title}</h2><p>${data.body}</p>`;
+    })
+    .catch(error => console.error("Error when fetching data:", error));
