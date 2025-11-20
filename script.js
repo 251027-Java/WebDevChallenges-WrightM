4ce0676db6f1d520a51259466d0f240b0a39cbc8
Marcus Wright <marcus03wright@icloud.com>
2025-11-20 11:29:30 -0600

    Completed task 26

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ function loadContent() {
     fetch(url + id)
     .then(response => response.json())
     .then(data => {
-        
+        if (!data.title) {
+            document.getElementById("output").innerHTML = "<p>No post found with that ID.</p>";
+        }
         document.getElementById("output").innerHTML = `<h3>${data.title}</h3><p>${data.body}</p>`;
     })
     .catch(error => console.error("Error when fetching data:", error));
