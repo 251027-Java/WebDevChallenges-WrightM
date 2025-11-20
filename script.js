eaffd9865b9f6acf15d6247ce33178f6a00c5ec8
Marcus Wright <marcus03wright@icloud.com>
2025-11-20 10:55:44 -0600

    Completed task 18

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-let input = document.getElementById("taskInput");
-let button = document.getElementById("addTask");
-let list = document.getElementById("taskList");
 
-button.addEventListener("click", function() {
-  let taskText = input.value.trim();
-  if (taskText === "") return;
 
-  let li = document.createElement("li");
-  let ch = document.createElement("input");
-  ch.type = "checkbox";
-  li.appendChild(ch);
-  li.appendChild(document.createTextNode(taskText + " "));
-
-  li.addEventListener("change", function() {
-    li.classList.toggle("done");
-  });
-
-  list.appendChild(li);
-  input.value = "";
-});
+square => a => a*a;
+console.log(square(5));
