450c63707aa656d70cdcd550ad6933abdf1c6ef3
Marcus Wright <marcus03wright@icloud.com>
2025-11-20 13:14:21 -0600

    Completed challenge 5

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-const url = "https://jsonplaceholder.typicode.com/posts/";
+let list = [];
 
-function loadContent() {
-    const id = document.getElementById('postId').value;
-    fetch(url + id)
-    .then(response => response.json())
-    .then(data => {
-        if (!data.title) {
-            document.getElementById("output").innerHTML = "<p>No post found with that ID.</p>";
-            return;
-        }
-        document.getElementById("output").innerHTML = `<h3>${data.title}</h3><p>${data.body}</p>`;
-    })
-    .catch(error => console.error("Error when fetching data:", error));
-};
-
-document.getElementById("fetchBtn").addEventListener("click", loadContent);
+function addToList(item) {
+    list.push(item);
+}
