1676f49fb387760de92699eed4dc40b7a50ea19d
Marcus Wright <marcus03wright@icloud.com>
2025-11-20 10:50:46 -0600

    Finished mini project 17.5

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function toggleDarkMode() {
-    document.body.classList.toggle("darkmode");
-}
+let input = document.getElementById("taskInput");
+let button = document.getElementById("addTask");
+let list = document.getElementById("taskList");
 
-document.getElementById("changeMode").addEventListener("click", toggleDarkMode);
+button.addEventListener("click", function() {
+  let taskText = input.value.trim();
+  if (taskText === "") return;
+
+  let li = document.createElement("li");
+  let ch = document.createElement("input");
+  ch.type = "checkbox";
+  li.appendChild(ch);
+  li.appendChild(document.createTextNode(taskText + " "));
+
+  li.addEventListener("change", function() {
+    li.classList.toggle("done");
+  });
+
+  list.appendChild(li);
+  input.value = "";
+});
